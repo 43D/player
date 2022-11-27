8777c99fba0c81740495abb8b2b9c8d31be13160
Allan Fel <allan.goioere@gmail.com>
2022-11-27 05:53:14 -0300

    Update Manifest

--- public/script/player/app.js
+++ public/script/player/app.js
@@ -1,6 +1,17 @@
 import { player } from "./player.js";
 
+let deferredPrompt;
+
 $(function () {
+
+    window.addEventListener('beforeinstallprompt', (e) => {
+        e.preventDefault();
+
+        deferredPrompt = e;
+
+//        btnAdd.style.display = "block";
+    });
+
     const playerClass = player();
     playerClass.init();
 });

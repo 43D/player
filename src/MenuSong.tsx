370402c9175ac2b478fdc74915d0cfd842ee649c
Allan <allan.goioere@hotmail.com>
2023-09-29 04:31:29 -0300

    Update test

---
+++ src/MenuSong.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MenuSong() {
+  return (
+    <div className="menu-song">
+      aa
+    </div>
+  );
+}
+
+export default MenuSong;
--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import MenuSong from './MenuSong';
 
 const main = ReactDOM.createRoot(document.getElementById('main') as HTMLElement
 );
 main.render(
   <React.StrictMode>
     <App />
+    <MenuSong />
   </React.StrictMode>
 );

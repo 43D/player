876388d0fa95ececc7eb44f1ab63f92caea2bd36
Allan <allan.goioere@hotmail.com>
2023-11-30 01:55:09 -0300

    404

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,12 +1,35 @@
-function NotFound() {
+import { useEffect, useState } from "react";
 
+function NotFound() {
+    const [backgroundImage, setBackgroundImage] = useState<string>('');
+    const [sourceImage, setSourceImage] = useState<string>('');
+
+
+    useEffect(() => {
+        fetch("https://43d.github.io/404/photos.json", {
+            method: 'GET',
+            redirect: 'follow'
+        })
+            .then(async response => randonly(await response.text()))
+    }, []);
+
+    const randonly = (text: string) => {
+        const data = JSON.parse(text);
+        const count = Object.keys(data.photos).length;
+        const r = Math.floor(Math.random() * (count));
+        const res = Object.values(data.photos)[r] as string[];
+        const src = "http://pixiv.net/artworks/" + res[0];
+        const image = "https://43d.github.io/404/image/" + res[0] + "_p" + res[1] + ".jpg";
+        setSourceImage(src);
+        setBackgroundImage("url(" + image + ")");
+    }
 
     return (
-        <div id="display-main" className="container-fluid displays">
-            <div className="App pt-2 pb-4">
-                <div className="row">
-                    404
-                </div>
+        <div id="display-image" style={{ backgroundImage: backgroundImage }}>
+            <div id="alert-404" className="position-absolute top-50 start-50 translate-middle px-5 py-1">
+                <p className="text-center m-1">404</p>
+                <p className="text-center m-0">Are you well?</p>
+                <p className="text-center m-1">souce link: <a href={sourceImage} target="_blank" rel="noopener noreferrer">here</a></p>
             </div>
         </div>
     );
